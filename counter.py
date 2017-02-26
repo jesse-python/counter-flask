fb01c4bc45b9bff4b62df81f3b3757164634e7f7
Jesse Naldo <naldojesse@jnaldoweb.com>
2017-02-25 17:00:03 -0800

    finished

--- counter.py
+++ counter.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'counter' not in session:
+        session['counter'] = 0
+    else:
+        session['counter'] += 1
+    return render_template('index.html', counter=session['counter'])
 
+@app.route('/plus2')
+def plus2():
+    print 'going'
+    if 'counter' not in session:
+        session['counter'] = 2
+    else:
+        session['counter'] += 2
+    return render_template('index.html', counter=session['counter'])
 
+@app.route('/reset')
+def reset():
+    if 'counter' not in session:
+        session['counter'] = 0
+    else:
+        session['counter'] = 0
+    return render_template('index.html', counter=session['counter'])
 app.run(debug=True)
